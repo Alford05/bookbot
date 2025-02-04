e92676c2cf3b136b4cabff7e5ca4b842351149d5
Oakley <dalford278@gmail.com>
2025-02-04 18:46:43 -0500

    finished

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-__name__ =="__main__"
+def main():
+    with open("books/frankenstein.txt") as f:
+            file_contents = f.read()
+    print(file_contents)
+
+if __name__ =="__main__":
+    main()
