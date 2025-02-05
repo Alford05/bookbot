9e9b76e78513bbe67be067b484d7aa12495b144b
Oakley <dalford278@gmail.com>
2025-02-04 19:15:04 -0500

    word count function 1.2

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def main():
-    word_count = 0
+    
     with open("books/frankenstein.txt") as f:
-            for line in f:
-                 words = line.split()
-                 word_count += len(words)
             file_contents = f.read()
     print(file_contents)
+    
+
+def words():
+    word_count = 0
+    with open("books/frankenstein.txt") as f:
+        for line in f:
+            words = line.split()
+            word_count += len(words)
     return word_count
 
 
